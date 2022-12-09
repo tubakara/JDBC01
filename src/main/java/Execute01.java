707e81b30ca24b0e4837322a9a18415c9f1e9786
tubakara <tugba.kara@gmail.com>
2022-12-10 01:43:34 +0300

    JDBC_01

---
+++ src/main/java/Execute01.java
@@ -0,0 +1,50 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Execute01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techpro","postgres","iderut");
+        Statement st = con.createStatement();
+        System.out.println("Connection Success");
+        /*
+        Execute() metodu hem DDL(CREATE, DROP,ALTER TABLE) VE DQL(SELECT) ICIN KULLANILIR
+        1) EGER EXECUTE() DDL icin false return yapar
+        2) eger execute() metodu DQL icin kullanilirsa data alirsak true alamazsak false verir yani ResultSet alindiginda true
+        aksi halde false veririr
+         */
+
+        //1.Örnek: "workers" adında bir table oluşturup "worker_id,worker_name, worker_salary" sütunlarını ekleyin.
+boolean sql1=st.execute("CREATE TABLE workers(worker_id VARCHAR(20), worker_name VARCHAR(20), worker_salary INT)");
+        System.out.println(sql1);// false return eder cunku data cagirmiyoruz
+
+        //2.Örnek: Table'a worker_address sütunu ekleyerek alter yapın.
+
+
+        //2. yol
+        String sql2="ALTER TABLE workers ADD worker_adres VARCHAR(80)";
+        boolean sql2b=st.execute(sql2);
+        System.out.println("sql2b="+sql2b);
+
+        //3. ornek workers table i silin
+
+        String sql3="Drop Table workers";
+        st.execute(sql3);
+
+        // baglantiyi kapama
+        con.close();
+        st.close();
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/ExecuteQuery01.java
@@ -0,0 +1,39 @@
+import javax.xml.transform.Result;
+import java.sql.*;
+
+public class ExecuteQuery01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techpro", "postgres", "iderut");
+        Statement st = con.createStatement();
+        //1. Örnek:  region id'si 1 olan "country name" değerlerini çağırın.
+
+        String sql1 = "select country_name from countries where region_id=1";
+        boolean r1 = st.execute(sql1);
+        System.out.println("r1 = " + r1);
+
+        //RECORDLARI GORMEK ICIN ExecuteQuery01() metodunu kullanmaliyiz
+        ResultSet resultSet1 = st.executeQuery(sql1);
+        while (resultSet1.next()) {
+            System.out.println(resultSet1.getString("country_name"));// buraya sutunun adi yazilir ya da numarasi
+        }
+        System.out.println("----------------");
+        //2.Örnek: "region_id"nin 2'den büyük olduğu "country_id" ve "country_name" değerlerini çağırın.
+
+        String sql2 = "SELECT country_name, country_id FROM countries WHERE region_id>2";
+        ResultSet resultSet2 = st.executeQuery(sql2);
+        while (resultSet2.next()) {
+            System.out.println(resultSet2.getString("country_name") + "--" + resultSet2.getString("country_id"));
+        }
+
+        System.out.println("******");
+        //3.Örnek: "number_of_employees" değeri en düşük olan satırın tüm değerlerini çağırın.
+        String sql3 = "SELECT * FROM companies WHERE number_of_employees=(SELECT MIN(number_of_employees) FROM companies)";
+        ResultSet resultSet3 = st.executeQuery(sql3);
+        while (resultSet3.next()) {
+            System.out.println(resultSet3.getInt(1) + "--- " + resultSet3.getString(2) + "---" + resultSet3.getInt(3));
+        }
+
+
+    }
+}
